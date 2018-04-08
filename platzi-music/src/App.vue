57cb7412b9142fcc25bce72724bb432155c55cd1
Javier <jaacu.97@gmail.com>
2018-04-08 08:58:05 -0400

    Inicial

---
+++ platzi-music/src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  #app
+    p(v-if="value") {{ val }}
+    p(v-else-if="false") {{ 'algo mas' }}
+    p(v-else) {{ 'lo ultimo' }}
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello World!',
+      value: false,
+      val: 'No se'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/main.scss'
+</style>
